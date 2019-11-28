aae0b786424a0cace0ce706e0f470960e30eb7a1
Eneri D' Angelis <eneridangelis@jacarape-011664.local>
2019-11-28 13:55:38 -0300

    student feature

--- app/src/main/java/com/example/questionarios/ui/activities/StudentActivity.java
+++ app/src/main/java/com/example/questionarios/ui/activities/StudentActivity.java
@@ -3,14 +3,42 @@ package com.example.questionarios.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.questionarios.R;
 
 public class StudentActivity extends AppCompatActivity {
 
+    EditText mInputAnswer;
+    Button mButtonSend;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+
+        initView();
+    }
+
+    private void initView() {
+        mInputAnswer = findViewById(R.id.input_answer);
+        mButtonSend = findViewById(R.id.button_send);
+
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onEnterClicked();
+            }
+        });
+
+    }
+
+    private void onEnterClicked() {
+        mInputAnswer.setText("");
+        Toast.makeText(this, R.string.answer_send, Toast.LENGTH_LONG).show();
     }
 }
